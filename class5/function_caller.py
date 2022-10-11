fe11cdad9fd1dd3ba7a2e3bd19c6fd4ec17f3e16
somename <a@gmail.com>
2022-10-11 16:03:23 +0000

    Function

---
+++ class5/function_caller.py
@@ -0,0 +1,23 @@
+#import statement
+from first_function import greetings
+from math_functions import multiplication
+from math_functions import addition
+from math_functions import division
+from math_functions import substraction
+import full_function
+from converter import convert_to_list, convert_to_str, convert_to_dict, convert_to_tuple
+from age_calculator import game
+
+
+
+
+greetings("Sarvar")
+multiplication(4,5)
+addition(3,8)
+division(9,3)
+substraction(4,2)
+FRUIT = ("apple","pear")
+convert_to_list(FRUIT)
+convert_to_str(10)
+game()
+full_function,full_function.function1()
